12097c4c2ed1778f484414cc05c91d010146b027
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-05 03:18:49 -0300

    :add: Adiciona o arquivo libLCS-0.0.40/lcs/exprtype.h

---
+++ libLCS-0.0.40/lcs/exprtype.h
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////////
+// This file is distributed as part of the libLCS library.
+// libLCS is C++ Logic Circuit Simulation library.
+//
+// Copyright (c) 2006-2007, B. R. Siva Chandra
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// In case you would like to contact the author, use the following e-mail
+// address: sivachandra_br@yahoo.com
+//////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __LCS_EXPR_TYPE_H__
+#define __LCS_EXPR_TYPE_H__
+
+namespace lcs
+{ // Start of namespace lcs.
+
+/// An enumeration which enumerates types for the various kinds of operations which can
+/// be used in an expression performing operations on lcs::InputBus (and hence, lcs::Bus)
+/// objects.
+enum ExprType
+{
+    /// Indicates an expression which consists of a single bus.
+    ///
+    BUS_EXPR               = 3072,
+
+    /// Indicates an expression which is a bitwise AND operation.
+    ///
+    BITWISE_AND_EXPR       = 3073,
+
+    /// Indicates an expression which is a bitwise OR operation.
+    ///
+    BITWISE_OR_EXPR        = 3074,
+
+    /// Indicates an expression which is a bitwise XOR operation.
+    ///
+    BITWISE_XOR_EXPR       = 3075,
+
+    /// Indicates an expression which is a one's complement operation.
+    ///
+    ONES_COMPLEMENT_EXPR   = 3076
+};
+
+} // End of namespace lcs.
+
+#endif // __LCS_EXPR_TYPE_H__
