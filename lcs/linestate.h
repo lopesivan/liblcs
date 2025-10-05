50d7981269123578f541b67b22f833f1944f7d20
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-05 03:43:26 -0300

    :style: Atualiza o estilo do arquivo lcs/linestate.h

--- lcs/linestate.h
+++ lcs/linestate.h
@@ -2,21 +2,21 @@
 // This file is distributed as part of the libLCS library.
 // libLCS is C++ Logic Circuit Simulation library.
 //
-// Copyright (c) 2006-2007, B. R. Siva Chandra
+// Copyright (c) 2006-2007, B. R. Siva Chandra, India
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-// This library is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 // In case you would like to contact the author, use the following e-mail
 // address: sivachandra_br@yahoo.com
@@ -45,33 +45,33 @@ enum LineState
 
     /// A state which denotes an unknown logic level.
     /// It is displayed on the screen as the character 'x'.
-    UNKNOWN               =  2,
+    UNKNOWN               = 'x',
 
     /// A state which denotes a high imepedence line.
     /// It is displayed on the screen as the character 'z'.
-    HIGH_IMPEDENCE        =  3
+    HIGH_IMPEDENCE        = 'z'
 };
 
 } // End of namespace lcs
 
-/// \fn
+/// \function
 /// An overloaded operator to perform a bitwise AND operation.
 ///
 const lcs::LineState operator&(const lcs::LineState& op1, const lcs::LineState& op2);
 
-/// \fn
+/// \function
 /// An overloaded operator to perform a bitwise OR operation.
 ///
 const lcs::LineState operator|(const lcs::LineState& op1, const lcs::LineState& op2);
 
-/// \fn
+/// \function
 /// An overloaded operator to perform a bitwise XOR operation.
 ///
 const lcs::LineState operator^(const lcs::LineState& op1, const lcs::LineState& op2);
 
-/// \fn
+/// \function
 /// An overloaded operator to perform a bitwise NOT operation.
 /// The opposite of the current line state is returned.
-const lcs::LineState operator~(const lcs::LineState &s);
+const lcs::LineState operator!(const lcs::LineState &s);
 
 #endif // __LCS_LINE_STATE_H__
