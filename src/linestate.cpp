7f10c7fe1c9e924428678e14a355982c9c050d4f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-05 03:43:29 -0300

    :style: Atualiza o estilo do arquivo src/linestate.cpp

--- src/linestate.cpp
+++ src/linestate.cpp
@@ -2,47 +2,55 @@
 // This file is distributed as part of the libLCS library.
 // libLCS is C++ Logic Circuit Simulation library.
 //
-// Copyright (c) 2006-2007, B. R. Siva Chandra
+// Copyright (c) 2006-2007, B. R. Siva Chandra, India
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-// This library is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 // In case you would like to contact the author, use the following e-mail
 // address: sivachandra_br@yahoo.com
 //////////////////////////////////////////////////////////////////////////////////
 
-#include <lcs/linestate.h>
+#include <linestate.h>
 
 using namespace lcs;
 using namespace std;
 
 const LineState operator&(const LineState& op1, const LineState& op2)
 {
-    if (op1 == LOW || op2 == LOW)
+    if (op1 == LOW && op2 == LOW)
+        return LOW;
+    else if (op1 == LOW && op2 == HIGH)
+        return LOW;
+    else if (op1 == HIGH && op2 == LOW)
         return LOW;
     else if (op1 == HIGH && op2 == HIGH)
         return HIGH;
     else
-        return LOW;
+        return UNKNOWN;
 }
 
 const LineState operator|(const LineState& op1, const LineState& op2)
 {
-    if (op1 == HIGH || op2 == HIGH)
-        return HIGH;
-    else
+    if (op1 == LOW && op2 == LOW)
         return LOW;
+    else if (op1 == LOW && op2 == HIGH)
+        return HIGH;
+    else if (op1 == HIGH && op2 == LOW)
+        return HIGH;
+    else //if (op1 == HIGH && op2 == HIGH)
+        return HIGH;
 }
 
 const LineState operator^(const LineState& op1, const LineState& op2)
@@ -53,16 +61,16 @@ const LineState operator^(const LineState& op1, const LineState& op2)
         return HIGH;
     else if (op1 == HIGH && op2 == LOW)
         return HIGH;
-    else
+    else //if (op1 == HIGH && op2 == HIGH)
         return LOW;
 }
 
-const lcs::LineState operator~(const lcs::LineState &s)
+const lcs::LineState operator!(const lcs::LineState &s)
 {
     if (s == HIGH)
         return LOW;
     else if (s == LOW)
         return HIGH;
     else
-        return HIGH;
+        return s;
 }
